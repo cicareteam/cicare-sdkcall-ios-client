9ae4885e937e99f76dd25bafc99fa78fc5f2f161
annaselh <annas.elh@gmail.com>
2025-08-15 16:46:57 +0700

    Initial Commit

---
+++ CiCareCall/CiCareCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiCareCallApp.swift
+//  CiCareCall
+//
+//  Created by Mohammad Annas Al Hariri on 15/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CiCareCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiCareCallTests/CiCareCallTests.swift
@@ -0,0 +1,17 @@
+//
+//  CiCareCallTests.swift
+//  CiCareCallTests
+//
+//  Created by Mohammad Annas Al Hariri on 15/08/25.
+//
+
+import Testing
+@testable import CiCareCall
+
+struct CiCareCallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
